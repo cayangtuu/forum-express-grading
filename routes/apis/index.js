64a234d4b69f6068d84320e989056bde89483ec6
cayang <cayang@simenvi.com.tw>
2023-03-08 11:54:36 +0800

    feat: add admin-services

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const admin = require('./modules/admin')
 const restaurantController = require('../../controllers/apis/restaurant-controller')
 
+router.use('/admin', admin)
 router.get('/restaurants', restaurantController.getRestaurants)
 
 module.exports = router
---
+++ services/admin-services.js
@@ -0,0 +1,13 @@
+const { Restaurant, Category } = require('../models')
+const adminController = {
+  getRestaurants: (req, cb) => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurants => cb(null, { restaurants }))
+      .catch(err => cb(err))
+  }
+}
+module.exports = adminController
