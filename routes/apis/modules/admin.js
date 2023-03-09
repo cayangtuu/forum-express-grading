e17ce36a4ea0713e11ab54074642b3ca017e04c8
cayang <cayang@simenvi.com.tw>
2023-03-09 17:29:59 +0800

    feat: api signin add jwt token

--- routes/apis/modules/admin.js
+++ routes/apis/modules/admin.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../../controllers/apis/admin-controller')
-const { apiErrorHandler } = require('../../../middleware/error-handler')
 const upload = require('../../../middleware/multer')
 
 router.delete('/restaurant/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
-
-router.use('/', apiErrorHandler)
 
 module.exports = router
